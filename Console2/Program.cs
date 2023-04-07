d2457158df62742cca5202e1aac2569f735e7ed0
Захарова Елизавета <93691972+Dinozaurr@users.noreply.github.com>
2023-04-07 13:37:18 +0300

    Add files via upload

---
+++ Console2/Program.cs
@@ -0,0 +1,4 @@
+﻿//8 задание 
+double radius = Convert.ToSByte(Console.ReadLine());
+double area = Math.PI * Math.Pow(radius, 2);
+Console.WriteLine(Math.Round(area,2));
---
+++ console1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            sbyte x = Convert.ToSByte(Console.ReadLine());
+            sbyte x2 = Convert.ToSByte(Console.ReadLine());
+            int y = Convert.ToInt32(Math.Max(x, x2));
+            Console.WriteLine(Math.Abs(y));
+
+            sbyte z = Convert.ToSByte(Console.ReadLine());
+            sbyte z1 = Convert.ToSByte(Console.ReadLine());
+            int c = Convert.ToInt32(Math.Min(z, z1));
+            Console.WriteLine(Math.Abs(c));
+
+            sbyte k = Convert.ToSByte(Console.ReadLine());
+            int e = Convert.ToInt32(Math.Sqrt(k));
+            Console.WriteLine(Math.Abs(e));
+
+            double t = Convert.ToDouble(Console.ReadLine());
+            int d = Convert.ToInt32(Math.Truncate(t));
+            Console.WriteLine(d);
+
+        }
+    }
+}
